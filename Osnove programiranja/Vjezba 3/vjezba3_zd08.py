71a678d4beb33b593d60ab5d133fbadd2a6743f0
gogozd <gogozd@gmail.com>
2017-11-13 21:33:44 +0100

    Add files via upload

---
+++ Osnove programiranja/Vjezba 3/vjezba3_zd08.py
@@ -0,0 +1,9 @@
+# Gordan Volarević, 09.11.2017.
+# Vježba 3, zadatak 8
+
+niz = input('Unesite niz znakova: ')
+
+if 'a' in niz:
+    print('Slovo a se nalazi u nizu znakova ', niz)
+else:
+    print('Slovo a se ne nalazi u nizu znakova ', niz)
