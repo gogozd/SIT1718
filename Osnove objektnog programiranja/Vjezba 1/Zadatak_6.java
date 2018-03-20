fe191218852ab84fd7e63df4af6a278fcf5a1fbd
gogozd <gogozd@gmail.com>
2018-03-20 23:20:31 +0100

    Add files via upload

---
+++ Osnove objektnog programiranja/Vjezba 1/Zadatak_6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Zadatak_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int N;
+		
+		do {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Unesite N: ");
+			N = sc.nextInt();
+			
+		} 
+		while(N != 158); {
+			System.out.println("Unesena je trazena vrijednost.");
+		}
+		
+		
+	}
+
+}
