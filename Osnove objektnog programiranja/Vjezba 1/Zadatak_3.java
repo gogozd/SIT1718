2bc747b99fd48b8c24b53087cbfd0c4552fe221c
gogozd <gogozd@gmail.com>
2018-03-20 23:15:23 +0100

    Add files via upload

---
+++ Osnove objektnog programiranja/Vjezba 1/Zadatak_3.java
@@ -0,0 +1,13 @@
+
+public class Zadatak_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int suma = 0;
+		
+		for (int i = 1; i <= 15; i++)  {
+			suma = suma + i;
+		}
+		System.out.println(suma);
+	}
+}
