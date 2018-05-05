c8eb91cc9926da108a3aaf6d1415d8f62b007586
gogozd <gogozd@gmail.com>
2018-05-06 01:53:48 +0200

    Add files via upload

---
+++ Osnove objektnog programiranja/Predavanja/T3/Professor.java
@@ -0,0 +1,9 @@
+
+// implementacija u Javi
+public class Professor {
+}
+
+class Student {	// druga klasa ne moze biti public, samo jedan public u cijeloj klasi
+				// Java dopusta samo jednu public klasu
+	public Professor[] lecturer;
+}
