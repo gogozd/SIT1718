5bdf07f307c5aff0e34382db916c42690e6b36d3
gogozd <gogozd@gmail.com>
2018-05-05 19:48:49 +0200

    Update App_vj_2.java

--- Osnove objektnog programiranja/Predavanja/T2/App_vj_2.java
+++ Osnove objektnog programiranja/Predavanja/T2/App_vj_2.java
@@ -4,20 +4,20 @@ public class App {
 	public static void main(String[] args) {
 		
 		int a = 10;
-		int b = a;	// referenca b na a
+		int b = a;			// referenca b na a
 
 		Automobil a1 = new Automobil();	// kreiramo objekt a1
-		a1.pokreniMotor();	// poziva se metoda pokreniMotor
-		Automobil a2 = a1;	// referenca na a1
+		a1.pokreniMotor();		// poziva se metoda pokreniMotor
+		Automobil a2 = a1;		// referenca na a1
 		a1.pokreniMotor();
 		a2.pokreniMotor();
 		
 	}
 
 		public static class Automobil {	// stvaramo klasu Automobil
-										// trebalo dodati static zbog greske i
-										// nije definiran konstruktor u klasi
-			int KS = 170;	// varijable klase
+						// trebalo dodati static zbog greske
+						// za klasu koju smo stvorili nismo definirali konstruktor
+			int KS = 170;		// varijable klase
 			int V = 3000;
 			String tip = "SUV";
 			
