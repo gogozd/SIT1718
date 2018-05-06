088dffc9f134fe7d91f926d742bdbdee2909ab94
gogozd <gogozd@gmail.com>
2018-05-06 02:02:27 +0200

    Add files via upload

---
+++ Osnove objektnog programiranja/Predavanja/T4/VanjskaKlasa.java
@@ -0,0 +1,10 @@
+// Vanjska klasa sa unutarnjom klasom
+public class VanjskaKlasa {
+	// atributi vanjske klase
+	class unutarnjaKlasa {	// vanjska klasa moze imati vise unutarnjih klasa
+		String infoUnutarnja(String info) {
+			return info;
+		}
+	}
+
+}
