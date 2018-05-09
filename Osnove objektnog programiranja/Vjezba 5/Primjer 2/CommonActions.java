49793b8a1470d5e31835b3f75966c98936d567ad
gogozd <gogozd@gmail.com>
2018-05-09 13:30:47 +0200

    Update CommonActions.java

--- Osnove objektnog programiranja/Vjezba 5/Primjer 2/CommonActions.java
+++ Osnove objektnog programiranja/Vjezba 5/Primjer 2/CommonActions.java
@@ -1,3 +1,8 @@
+// U ovom zadatku radit ćete sa sučeljima. Pretpostavimo da imate dvije klase i to klasu Robot, te drugu Person koje nisu iz iste
+// porodice. Robot i Person imaju neka ponašanja koja su slična, ali istovremeno bitno različita → talk(), walk(), think(), calculate().
+// Kao što očekujete imaju neke metode koje su različite i njih ćemo istaknuti u prikazu dijagrama klasa. Sva ponašanja potebno je
+// realizirati korištenjem sučelja CommonActions.
+
 
 public interface CommonActions {
 
