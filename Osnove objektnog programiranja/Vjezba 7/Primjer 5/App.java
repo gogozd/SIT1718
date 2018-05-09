6972ea4ccc1c33b38a18fdc7e0e10509b20565c4
gogozd <gogozd@gmail.com>
2018-05-09 13:23:19 +0200

    Update App.java

--- Osnove objektnog programiranja/Vjezba 7/Primjer 5/App.java
+++ Osnove objektnog programiranja/Vjezba 7/Primjer 5/App.java
@@ -1,3 +1,9 @@
+// Načinite vezanu listu vozila i popunite je sa sljedećim elementima: car, helicopeter, electronic bike, truck, motorcycle, carriage
+// Vezanu listu vozila je potrebno ispisati upotrebom for each petlje u zasebnoj metodi glavnog programa koju ćete nazavti printList.
+// Primjenom while petlje i Iteratora (pogledajte API za sučelje Iterator) za listu vozila ispišite sve elemente liste (dobit ćete
+// isti rezultat kao s printList), ali istovremeno provjeravajte listu sadrži li element "carriage". Ako sadrži izbrišite taj
+// element, a u protivnom lista ostaje ista. Nakon petlje while ponovno ispišite listu.
+
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
