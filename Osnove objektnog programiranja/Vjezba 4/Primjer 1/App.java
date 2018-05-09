d9d171d12be1195b7a335248fef54c7b64e87512
gogozd <gogozd@gmail.com>
2018-05-09 12:49:16 +0200

    Add files via upload

---
+++ Osnove objektnog programiranja/Vjezba 4/Primjer 1/App.java
@@ -0,0 +1,13 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vehicle h1 = new Vehicle();
+		
+		h1.info();
+		System.out.println(h1.info());
+		System.out.println(getbrand());
+	}
+
+}
